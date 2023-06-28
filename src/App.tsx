5849d98db0c56ec0490d30140ed064ea127593fe
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-29 01:57:14 +0900

    feat: 라이브러리 없이 구글 지도 로드

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useRef } from 'react';
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
 
 function App() {
+  const mapElement = useRef(null);
+
+  const loadScript = useCallback((url: string) => {
+    const firstScript = window.document.getElementsByTagName('script')[0];
+    const newScript = window.document.createElement('script');
+    newScript.src = url;
+    newScript.async = true;
+    newScript.defer = true;
+    firstScript?.parentNode?.insertBefore(newScript, firstScript);
+  }, []);
+
+
+  const initMap = useCallback(() => {
+    const { google } = window;
+    if (!mapElement.current || !google) return;
+
+
+    const location = { lat: 37.5656, lng: 126.9769 };
+    const map = new google.maps.Map(mapElement.current, {
+      zoom: 17,
+      center: location,
+    });
+    new google.maps.Marker({
+      position: location,
+      map,
+    });
+  }, []);
+
+
+  useEffect(() => {
+    const script = window.document.getElementsByTagName('script')[0];
+    const includeCheck = script.src.startsWith(
+      'https://maps.googleapis.com/maps/api'
+    );
+
+
+    if (includeCheck) return initMap();
+
+
+    window.initMap = initMap;
+    loadScript(
+      `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&callback=initMap&language=ko`
+    );
+  }, [initMap, loadScript]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      ref={mapElement}
+      style={{
+        minHeight: '100vh'
+      }}
+    />
   );
 }
+
 
 export default App;
