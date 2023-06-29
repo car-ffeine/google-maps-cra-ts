900a00f4657557500d2789303370b8ecfa95ca05
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-29 23:20:23 +0900

    refactor: GoogleMap 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,9 @@
-import { useCallback, useEffect, useRef } from 'react';
-
-declare global {
-  interface Window {
-    initMap: () => void;
-  }
-}
+import GoogleMap from './GoogleMap';
 
 function App() {
-  const mapElement = useRef(null);
+  const location = { lat: 37.5656, lng: 126.9769 };
 
-  const loadScript = useCallback((url: string) => {
-    const firstScript = window.document.getElementsByTagName('script')[0];
-    const newScript = window.document.createElement('script');
-    newScript.src = url;
-    newScript.async = true;
-    newScript.defer = true;
-    firstScript?.parentNode?.insertBefore(newScript, firstScript);
-  }, []);
-
-
-  const initMap = useCallback(() => {
-    const { google } = window;
-    if (!mapElement.current || !google) return;
-
-
-    const location = { lat: 37.5656, lng: 126.9769 };
-    const map = new google.maps.Map(mapElement.current, {
-      zoom: 17,
-      center: location,
-    });
-    new google.maps.Marker({
-      position: location,
-      map,
-    });
-  }, []);
-
-
-  useEffect(() => {
-    const script = window.document.getElementsByTagName('script')[0];
-    const includeCheck = script.src.startsWith(
-      'https://maps.googleapis.com/maps/api'
-    );
-
-
-    if (includeCheck) return initMap();
-
-
-    window.initMap = initMap;
-    loadScript(
-      `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&callback=initMap&language=ko`
-    );
-  }, [initMap, loadScript]);
-
-
-  return (
-    <div
-      ref={mapElement}
-      style={{
-        minHeight: '100vh'
-      }}
-    />
-  );
+  return <GoogleMap apiKey={`${process.env.REACT_APP_API_KEY}`} location={location} />;
 }
-
 
 export default App;
---
+++ src/GoogleMap.tsx
@@ -0,0 +1,58 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+interface GoogleMapProps {
+  apiKey: string;
+  location: { lat: number; lng: number };
+}
+
+function GoogleMap({ apiKey, location }: GoogleMapProps) {
+  const mapElement = useRef(null);
+
+  const loadScript = useCallback((url: string) => {
+    const firstScript = window.document.getElementsByTagName('script')[0];
+    const newScript = window.document.createElement('script');
+    newScript.src = url;
+    newScript.async = true;
+    newScript.defer = true;
+    firstScript?.parentNode?.insertBefore(newScript, firstScript);
+  }, []);
+
+  const initMap = useCallback(() => {
+    const { google } = window;
+    if (!mapElement.current || !google) return;
+
+    const map = new google.maps.Map(mapElement.current, {
+      zoom: 17,
+      center: location,
+    });
+    new google.maps.Marker({
+      position: location,
+      map,
+    });
+  }, [location]);
+
+  useEffect(() => {
+    const script = window.document.getElementsByTagName('script')[0];
+    const includeCheck = script.src.startsWith(
+      'https://maps.googleapis.com/maps/api'
+    );
+
+    if (includeCheck) return initMap();
+
+    window.initMap = initMap;
+    loadScript(
+      `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap&language=ko`
+    );
+  }, [apiKey, initMap, loadScript]);
+
+  return (
+    <div
+      ref={mapElement}
+      style={{
+        minHeight: '100vh',
+      }}
+    />
+  );
+};
+
+export default GoogleMap;
