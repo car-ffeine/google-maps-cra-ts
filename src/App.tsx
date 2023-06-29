1e96d1a219f28252ae5e88c64b1ceab248344c44
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-06-29 23:43:11 +0900

    feat: 상태로 이동하는 예제 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
+import { useState } from 'react';
 import GoogleMap from './GoogleMap';
 
 function App() {
-  const location = { lat: 37.5656, lng: 126.9769 };
+  const [location, setLocation] = useState({ lat: 37.5656, lng: 126.9769 });
   const mapStyle = {
-    minHeight: '100vh',
+    minHeight: '70vh',
   };
   const zoom = 17;
 
   return (
-    <GoogleMap
-      apiKey={`${process.env.REACT_APP_API_KEY}`}
-      location={location}
-      style={mapStyle}
-      zoom={zoom}
-    />
+    <>
+      <GoogleMap
+        apiKey={`${process.env.REACT_APP_API_KEY}`}
+        location={location}
+        style={mapStyle}
+        zoom={zoom}
+      />
+      <button onClick={() => setLocation({ lat: 37.5656, lng: 126.9769 })}>이동</button>
+    </>
+
   );
 }
 
